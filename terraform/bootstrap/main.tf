cc83e39e2852658d2675fde961aa37dd5096ec1b
Yash Jagani <yashjagani17@gmail.com>
2026-05-21 02:45:03 +0100

    actions test

--- terraform/bootstrap/main.tf
+++ terraform/bootstrap/main.tf
@@ -16,8 +16,14 @@ variable "aws_profile" {
   default = "yash"
 }
 
-data "aws_iam_openid_connect_provider" "github" {
-  url = "https://token.actions.githubusercontent.com"
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [
+    "9514f4ed3c841c96c43def0f0acbf177405ded12",
+    "6938fd4d98bab03faadb97b34396831e3780aea1",
+    "1c58a3a8518e8759bf075b76b750d4f2df264fcd",
+  ]
 }
 
 resource "aws_iam_role" "github_actions" {
@@ -28,7 +34,7 @@ resource "aws_iam_role" "github_actions" {
     Statement = [{
       Effect = "Allow"
       Principal = {
-        Federated = data.aws_iam_openid_connect_provider.github.arn
+        Federated = aws_iam_openid_connect_provider.github.arn
       }
       Action = "sts:AssumeRoleWithWebIdentity"
       "Condition": {
